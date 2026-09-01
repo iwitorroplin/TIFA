a5ebd7e2df918910938ae4c9760f475000b431fc
iwitorroplin <iwitorroplin@gmail.com>
2026-09-01 15:00:52 +0200

    feat(steriflow): Update tab bar with meaningful tabs and icons

--- src/ui/views/steriflow/tab_bar.py
+++ src/ui/views/steriflow/tab_bar.py
@@ -1,3 +1,9 @@
+from src.ui.assets import (
+    APP_ICON,
+    MATERIA_RED_ICON,
+    MATERIA_GREEN_ICON,
+    MATERIA_YELLOW_ICON,
+)
 from src.ui.components.tab_bar import TabBar
 
 
@@ -5,5 +11,7 @@ class SteriflowTabBar(TabBar):
     def __init__(self):
         super().__init__()
 
-        self.add_item("Tab 1")
-        self.add_item("Tab 2")
+        self.add_item("Home", APP_ICON)
+        self.add_item("Configuración", MATERIA_RED_ICON)
+        self.add_item("Data", MATERIA_RED_ICON)
+        self.add_item("Logs", MATERIA_RED_ICON)
