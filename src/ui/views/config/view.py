e1b146eadfe3f489a6e0543d2810fb43c64d2ca9
iwitorroplin <iwitorroplin@gmail.com>
2026-09-01 15:00:53 +0200

    feat(config): Add "Start with Windows" checkbox

--- src/ui/views/config/view.py
+++ src/ui/views/config/view.py
@@ -1,6 +1,10 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QCheckBox, QVBoxLayout, QWidget
 
 
 class ConfigView(QWidget):
     def __init__(self):
         super().__init__()
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(QCheckBox("Iniciar TIFA al iniciar sesión de Windows"))
+        layout.addStretch()
